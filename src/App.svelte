aecc050c21d581349bac0d99dc6424bbfce927e7
Mayar Osama <mayarosama18@gmail.com>
2022-05-15 18:14:58 +0200

    Card Transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import CardList from "./components/CardList.svelte";
+  import Map from "./components/Map.svelte";
   //   export let name;
 </script>
 
 <main>
+  <Map />
   <CardList />
 </main>
 
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -5,12 +5,11 @@
   <slot />
 </div>
 
-<style>
+<!-- <style>
   .card {
     display: inline-block;
     border-radius: 10px;
     padding: 0 1rem;
-    /* box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; */
     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
     padding-left: 3rem;
     background-color: aquamarine;
@@ -24,12 +23,5 @@
     padding-left: 0px;
   }
 
-  /* @keyframes main {
-    0% {
-      transform: scaleX(0);
-    }
-    100% {
-      transform: scaleX(1);
-    }
-  } */
-</style>
+
+</style> -->
